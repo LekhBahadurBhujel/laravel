5838b53e79d43f930338c67ec7b7629ed250de9a
Lekh Bhujel <lekhbdrbhujel@gmail.com>
2025-06-16 18:29:14 +0545

    new commit

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
